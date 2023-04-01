b19ad9019b4fc7bd6d915cc7b5a1cbb864ee86ec
jeremykissack <kissackjeremy@gmail.com>
2023-04-01 11:08:31 -0700

    updates to reddit crawler and prompt

--- redditCrawler.py
+++ redditCrawler.py
@@ -15,16 +15,26 @@ reddit = praw.Reddit(client_id=client_id,
                      client_secret=client_secret,
                      user_agent=user_agent)
 
-def get_top_headlines(subreddit_name, time_filter, limit):
+def get_top_headlines_and_comments(subreddit_name, time_filter, post_limit, comment_limit):
     subreddit = reddit.subreddit(subreddit_name)
-    top_posts = subreddit.top(time_filter=time_filter, limit=limit)
+    top_posts = subreddit.top(time_filter=time_filter, limit=post_limit)
     
     for post in top_posts:
         print(f'Title: {post.title}\nURL: {post.url}\nScore: {post.score}\n')
+        
+        post.comments.replace_more(limit=0)
+        top_comments = post.comments.list()[:comment_limit]
+        
+        for i, comment in enumerate(top_comments, start=1):
+            print(f'Comment {i}:')
+            print(comment.body)
+            print('\n')
+        print('-' * 80)
 
 # Change 'all' to a specific subreddit name if you want top headlines from a specific subreddit.
 subreddit_name = 'all'
-time_filter = 'day' # 'day' for top headlines of the day
-limit = 10 # Number of headlines to fetch
+time_filter = 'week' # 'day' for top headlines of the day
+post_limit = 10 # Number of headlines to fetch
+comment_limit = 3 # Number of top comments to fetch per post
 
-get_top_headlines(subreddit_name, time_filter, limit)
+get_top_headlines_and_comments(subreddit_name, time_filter, post_limit, comment_limit)
--- utils.py
+++ utils.py
@@ -1,5 +1,16 @@
 SYSTEM_MESSAGE = "You are a helpful assistant that generates long and creative meme prompts and short but funny texts."
-PROMPT = "Generate a meme prompt and text for a really edgy meme (it should be low effort, but kinda stupid, and funny because it stupid), along with a caption and around 10 hashtags for the post. A meme prompt is used to generate the meme base image with DALL·E 2, and the text part is intended to be overlayed on the image, completing the meme. Use the format: 'Meme Prompt: [prompt] | Text: [text] | Caption: [caption] | Hashtags: [hashtags]'."
+
+def generate_prompt(title: str, flair: str, score: int, post_text: str, top_comments) -> str:
+    return f"Generate a meme prompt and text for a really edgy meme based on the top Reddit post of the day. \
+        Title: {title}. Flair: {flair}. Score: {score}. Post text: {post_text}. Top comments: {', '.join(top_comments)}. \
+        It should be low effort, but kinda stupid, and funny because it's stupid. \
+        Dark humour is good, but it should not violate openAI's content policies. \
+        Try to make the meme absolutely unhinged. \
+        Also make a caption and around 20 hashtags for the post. \
+        A meme prompt is used to generate the meme base image with DALL·E 2, \
+            and the text part is intended to be overlaid on the image, completing the meme. \
+        Use the format: 'Meme Prompt: [prompt] | Text: [text] | Caption: [caption] | Hashtags: [hashtags]'."
+
 
 def draw_text_with_border(draw, text, position, font, text_color, border_color, border_thickness):
     x, y = position
